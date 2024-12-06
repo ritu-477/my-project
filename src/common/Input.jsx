adb31b1c5522171e59fa95de862576def746b3a1
ritu-477 <ritujoharbishnoi@gmail.com>
2024-12-06 22:08:26 +0530

    done

--- src/common/Input.jsx
+++ src/common/Input.jsx
@@ -2,7 +2,7 @@
 const Input = ({ text, type = "text", classStyle }) => {
     return (
             <input
-                className={`font-normal sm:text-base sm:leading-custom-sm text-sm leading-4 font-helvetica opacity-50 outline-none placeholder:text-deep-blue text-deep-blue ${classStyle}`}
+                className={`font-normal w-full sm:text-base sm:leading-custom-sm text-sm leading-4 font-helvetica opacity-50 outline-none placeholder:text-deep-blue text-deep-blue ${classStyle}`}
                 type={type}
                 placeholder={text}
             />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,9 +15,9 @@ const Hero = () => {
           <div className='flex flex-col lg:w-[612px] w-full max-lg:justify-center max-lg:items-center'>
             <h1 className='font-helvetica sm:text-5xl text-3xl xl:text-custom-6xl xl:leading-custom-6xl text-white lg:text-start text-center lg:max-w-[612px]'>Easily Compare <span className='font-bold'>Energy, Gas, And Internet</span> Plans</h1>
             <Description classStyle='text-white lg:max-w-[506px] w-full max-lg:text-center pt-4 opacity-90 pb-6 lg:pb-[42px]' text={'Find and switch to the best energy, gas, and internet plans with ease. Our platform simplifies your search, letting you compare providers and make informed decisions, all in one place.'} />
-            <div className='rounded-tr-[100px] rounded-br-[100px] rounded-bl-[70px] bg-white flex justify-between max-w-[476px] w-full py-[9px] ps-4 sm:ps-[22px] pr-[9px] border border-gray'>
+            <div className='rounded-tr-[100px] rounded-br-[100px] rounded-bl-[70px] bg-white flex justify-between max-w-[476px] w-full py-2 ps-4 sm:ps-[21pz] pr-2 border border-gray'>
               <Input classStyle={'font-normal font-helvetica outline-none !placeholder:text-deep-blue text-deep-blue'} text={'Start typing your address'} type='text' />
-              <CustomButton classStyle="bg-sky-blue text-white" text="Compare" iconColor="white" />
+              <CustomButton classStyle="bg-sky-blue text-white ms-2" text="Compare" iconColor="white" />
             </div>
           </div>
           <div className='relative flex max-lg:justify-center mt-10 lg:mt-0'>
